63c7ff4682275b5e379918d216b9138d5a6c8838
AllenLu <allenluuu@foxmail.com>
2022-11-16 00:22:15 +0800

    fix(backend, site): change SiteListDto class name

--- backend/src/site/dto/site.dto.ts
+++ backend/src/site/dto/site.dto.ts
@@ -1,3 +1,3 @@
-export class AccountListDto {
+export class SiteListDto {
     searchText: string;
 }
--- backend/src/site/site.controller.ts
+++ backend/src/site/site.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { Cookies } from 'src/cookie.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { AccountListDto } from './dto/site.dto';
+import { SiteListDto } from './dto/site.dto';
 import { SiteDocument } from './schemas/site.schema';
 import { SiteService } from './site.service';
 
@@ -13,8 +13,8 @@ export class SiteController {
   @Post('list')
   async list(
     @Cookies('id') id: string,
-    @Body() accountListDto: AccountListDto,
+    @Body() siteListDto: SiteListDto,
   ): Promise<SiteDocument[]> {
-    return this.siteService.list(accountListDto.searchText, id);
+    return this.siteService.list(siteListDto.searchText, id);
   }
 }
