ae09b374b08d93fd12431be3e03c2c4160f8958b
AllenLu <allenluuu@foxmail.com>
2022-12-25 23:39:18 +0800

    fix(frontend, deivce): disable light slider when light is off

--- frontend/src/components/deivces/Light.tsx
+++ frontend/src/components/deivces/Light.tsx
@@ -41,6 +41,7 @@ export default function Light({
             onLuminanceChange(e);
           }}
           width="70%"
+          isDisabled={!isOn}
         >
           <SliderTrack>
             <SliderFilledTrack />
